dd92baa6e07d2fa74b8185221f508f42bcda02c4
Geet Sethi <geetsethi05@gmail.com>
2023-11-08 21:23:37 +0530

    feat: add a parallax effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,46 @@
+import { useEffect, useState } from "react";
 import bgImg from "../assets/background.png";
 import fgImg from "../assets/foreground.png";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
-      <img src={bgImg} alt="background" className={styles.layer1} />
-      <h1>Sustain</h1>
-      <img src={fgImg} alt="foreground" className={styles.layer3} />
+      <img
+        src={bgImg}
+        alt="background"
+        className={styles.layer1}
+        style={{
+          transform: `translateY(-${scrollPosition * 0.01}px)`,
+          filter: `blur(${scrollPosition * 0.005}px)`,
+        }}
+      />
+      <h1 style={{ transform: `translate(-50%, -${scrollPosition}px)` }}>
+        Sustain
+      </h1>
+      <img
+        src={fgImg}
+        alt="foreground"
+        className={styles.layer3}
+        style={{
+          transform: `translateY(${scrollPosition * 0.05}px)`,
+          filter: `blur(${scrollPosition * 0.005}px)`,
+        }}
+      />
       <div className={styles.bottom_gradient} />
     </div>
   );
